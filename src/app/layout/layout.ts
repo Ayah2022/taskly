dab127474c65dd761392698aadff490b1806d674
Ayah2022 <aya.rateb2022@yahoo.com>
2026-09-14 22:57:47 +0300

    style/ TM-06 layout design

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
+import { Header } from './header/header';
+import { Sidebar } from './sidebar/sidebar';
 @Component({
-  imports: [],
   selector: 'app-layout',
   styleUrl: './layout.css',
   templateUrl: './layout.html',
+  imports: [RouterOutlet, Header, Sidebar],
 })
 export class Layout {}
